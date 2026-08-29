db6d28b828e344f8581cb4115b299302df059bfd
Vihaan Angaria <vihaanangaria@gmail.com>
2026-08-29 22:18:24 +0530

    chore: bump app version to 1.2.3 and version code to 152

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "echo.music.***REMOVED***"
         minSdk = 26
         targetSdk = 36
-        versionCode = 151
-        versionName = "1.2.2"
+        versionCode = 152
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
